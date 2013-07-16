49b10ff91e305833539301b2fee862b04c56027f
miguelgazela <miguel.gazela@gmail.com>
2013-07-16 09:53:09 +0100

    First project - Palindrome check

---
+++ Text/palindrome.py
@@ -0,0 +1,18 @@
+# Check if Palindrome
+# Checks if the string entered by the user is a palindrome.
+# That is that it reads the same forwards as backwards like “racecar”
+
+
+def is_palindrome(string):
+    if len(string) <= 1:
+        return True
+    if string[0] != string[-1]:
+        return False
+    return is_palindrome(string[1:-1])
+
+string = raw_input('String: ').lower()
+
+if is_palindrome(string):
+    print string + ' is a palindrome'
+else:
+    print string + ' is not a palindrome'

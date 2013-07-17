49626493e1efebb22be1d01cd90308859bd5a891
miguelgazela <miguel.gazela@gmail.com>
2013-07-17 14:00:13 +0100

    Improved version

--- Text/count_words.py
+++ Text/count_words.py
@@ -2,16 +2,27 @@
 # Counts the number of individual words in a string.
 # For added complexity read these strings in from a text file and generate a summary.
 
-fin = open("count_words.txt")
+import string
 
-words = {}
 
-for line in fin:
-    line = line.strip()
-    line_words = line.split()
+def process_file(filename):
+    hist = {}
+    fin = open(filename)
+    for line in fin:
+        process_line(line, hist)
+    return hist
 
-    for word in line_words:
-        words[word] = words.get(word, 0) + 1
 
-print words
+def process_line(line, hist):
+    line = line.replace('-', ' ')
+
+    for word in line.split():
+        word = word.strip(string.punctuation+string.whitespace)
+        word = word.lower()
+
+        hist[word] = hist.get(word, 0) + 1
+
+if __name__ == "__main__":
+    hist = process_file("count_words.txt")
+    print hist
 
